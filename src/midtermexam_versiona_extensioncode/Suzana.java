3b3cca6612e2e0564987b2f3b97a1fd5ba6a1467
Suzana Guleva <guleva@sheridancollege.ca>
2023-03-08 16:11:39 -0500

    New branch and java file created

---
+++ src/midtermexam_versiona_extensioncode/Suzana.java
@@ -0,0 +1,14 @@
+package midtermexam_versiona_extensioncode;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author suzanaguleva
+ */
+public class Suzana {
+    //New branch created
+}
